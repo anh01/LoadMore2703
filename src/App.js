acf4dca8a8d34ce1970c4845690b43f18e9c274d
Van Pho Nguyen <vanpho02@gmail.com>
2017-05-08 19:06:50 +0700

    ket thuc

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, ListView, StatusBar } from 'react-native';
+import { View, Text, ListView, StatusBar, RefreshControl } from 'react-native';
 
 StatusBar.setHidden(true);
 
@@ -8,26 +8,51 @@ export default class App extends Component {
         super(props);
         const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
         this.state = {
-            listColor: ds
+            listColor: ds,
+            refreshing: false,
+            page: 1
         };
+        this.arr = [];
     }
 
     componentDidMount() {
         fetch('http://localhost:3000/products/1')
-        .then(res => res.json())
-        .then(resJSON => this.setState({ 
-            listColor: this.state.listColor.cloneWithRows(resJSON) 
-        }))
+            .then(res => res.json())
+            .then(resJSON => {
+                this.arr = [...resJSON, ...this.arr];
+                this.setState({
+                    listColor: this.state.listColor.cloneWithRows(this.arr)
+                })
+            })
     }
 
     render() {
         return (
             <View style={{ backgroundColor: 'lightblue', flex: 1 }}>
-                <ListView 
+                <ListView
                     dataSource={this.state.listColor}
                     renderRow={color => (
                         <Text style={{ color: 'gray', fontSize: 40, margin: 30 }}>{color}</Text>
                     )}
+                    refreshControl={
+                        <RefreshControl
+                            refreshing={this.state.refreshing}
+                            onRefresh={() => {
+                                this.setState({ refreshing: true });
+                                const newPage = this.state.page + 1;
+                                fetch(`http://localhost:3000/products/${newPage}`)
+                                    .then(res => res.json())
+                                    .then(resJSON => {
+                                        this.arr = [...resJSON, ...this.arr];
+                                        this.setState({
+                                            refreshing: false,
+                                            listColor: this.state.listColor.cloneWithRows(this.arr),
+                                            page: newPage
+                                        });
+                                    });
+                            }}
+                        />
+                    }
                 />
             </View>
         );
