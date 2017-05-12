2c398c2e973e6bc8c28b2fbcf8c8ecc7e2d4ddc5
Van Pho Nguyen <vanpho02@gmail.com>
2017-05-12 18:22:16 +0700

    db

--- server/db.js
+++ server/db.js
@@ -13,16 +13,44 @@ const config = {
 const pool = new pg.Pool(config);
 
 function queryDB(sql, cb) {
-    pool.connect((err, client) => {
+    pool.connect((err, client, done) => {
         if (err) return cb(err, undefined);
         client.query(sql, (errQuery, result) => {
-            if (errQuery) return cb(errQuery, undefined)
+            done(errQuery);
+            if (errQuery) return cb(errQuery, undefined);
             cb(undefined, result);
         });
     });
 }
 
-const kq = queryDB('SELECT * FROM "Product"', function (err, result) {
-    if (err) return console.log(err);
-    console.log(result.rows);
-});
+// const kq = queryDB('SELECT * FROM "Product"', function (err, result) {
+//     if (err) return console.log(err);
+//     console.log(result.rows);
+// });
+
+/*
+    1 -> 123
+    2 -> 456
+    3 -> 789
+*/
+
+// function getProducts(page, cb) {
+//     const min = (page * 3) - 2;
+//     const max = page * 3;
+//     const sql = `SELECT * FROM "Product" WHERE id >= ${min} AND id <= ${max}`;
+//     queryDB(sql, (err, result) => {
+//         if (err) return cb(err);
+//         cb(undefined, result.rows);
+//     });
+// }
+
+function getProducts(idMax, cb) {
+    const sql = `SELECT * FROM "Product" WHERE id > ${idMax} 
+    ORDER BY id ASC LIMIT 3`;
+    queryDB(sql, (err, result) => {
+        if (err) return cb(err);
+        cb(undefined, result.rows);
+    });
+}
+
+module.exports = getProducts;
--- server/index.js
+++ server/index.js
@@ -1,47 +1,15 @@
 const express = require('express');
+const getProducts = require('./db');
 
 const app = express();
 app.listen(3000, () => console.log('Server started'));
 
 app.get('/', (req, res) => res.send('Server is running!!!'));
 
-app.get('/products/:page', (req, res) => {
-    //page - 1 => 0 - 4.
-    //page - 2 => 5 - 9.
-    //page - 3 => 10 - 14.
-    const { page } = req.params;
-    const startIndex = (page - 1) * 5;
-    const stopIndex = page * 5;
-    const result = CSS_COLOR_NAMES.slice(startIndex, stopIndex);
-    res.send(result);
+app.get('/products/:idMax', (req, res) => {
+    const { idMax } = req.params;
+    getProducts(idMax, (err, result) => {
+        if (err) return res.send(err);
+        res.send(result);
+    });
 });
-
-const CSS_COLOR_NAMES = [
-    "AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure",
-    "Beige","Bisque","Black","BlanchedAlmond","Blue",
-    "BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse",
-    "Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson",
-    "Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray",
-    "DarkGrey","DarkGreen","DarkKhaki","DarkMagenta",
-    "DarkOliveGreen","Darkorange","DarkOrchid","DarkRed",
-    "DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray",
-    "DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink",
-    "DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick",
-    "FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite",
-    "Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew",
-    "HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush",
-    "LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan",
-    "LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink",
-    "LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey",
-    "LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon",
-    "MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen",
-    "MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed",
-    "MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace",
-    "Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen",
-    "PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum",
-    "PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon",
-    "SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue",
-    "SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal",
-    "Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke",
-    "Yellow","YellowGreen"
-    ];
