e2a3756a7982c0fb04ddd861dc2ea48a2c25ece0
Van Pho Nguyen <vanpho02@gmail.com>
2017-05-22 18:12:22 +0700

    UI

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React, { Component } from 'react';
-import { View, StatusBar } from 'react-native';
-import { Provider } from 'react-redux';
-import ComA from './components/ComA';
-import store from './redux/store';
+import { StatusBar } from 'react-native';
+import Authentication from './components/Authentication';
 
 StatusBar.setHidden(true);
 
 export default class App extends Component {
     render() {
-        return (
-            <Provider store={store}>
-                <View style={{ flex: 1 }}>
-                    <ComA />
-                </View>
-            </Provider>
-        );
+        return <Authentication />;
     }
 }
---
+++ src/components/SignUp.js
@@ -0,0 +1,117 @@
+import React, { Component } from 'react';
+import { View, TextInput, Text, TouchableOpacity, StyleSheet, Alert } from 'react-native';
+
+export default class SignUp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            password: '',
+            rePassword: '',
+            phone: '',
+            address: ''
+        };
+    }
+
+    onSuccess() {
+        Alert.alert(
+            'Notice',
+            'Sign up successfully',
+            [
+                { text: 'OK', onPress: this.props.gotoSignIn() }
+            ],
+            { cancelable: false }
+        );
+    }
+
+    onFail() {
+        Alert.alert(
+            'Notice',
+            'Email has been used by other',
+            [
+                { text: 'OK', onPress: () => this.removeEmail.bind(this) }
+            ],
+            { cancelable: false }
+        );
+    }
+
+    removeEmail() {
+        this.setState({ email: '' });
+    }
+
+
+    render() {
+        const { inputStyle, bigButton, buttonText } = styles;
+        return (
+            <View>
+                <TextInput 
+                    style={inputStyle} 
+                    placeholder="Enter your name" 
+                    value={this.state.name}
+                    onChangeText={text => this.setState({ name: text })}
+                />
+                <TextInput 
+                    style={inputStyle} 
+                    placeholder="Enter your email" 
+                    value={this.state.email}
+                    onChangeText={text => this.setState({ email: text })}
+                />
+                <TextInput 
+                    style={inputStyle} 
+                    placeholder="Enter your password" 
+                    value={this.state.password}
+                    secureTextEntry
+                    onChangeText={text => this.setState({ password: text })}
+                />
+                <TextInput 
+                    style={inputStyle} 
+                    placeholder="Re-enter your password" 
+                    value={this.state.rePassword}
+                    secureTextEntry
+                    onChangeText={text => this.setState({ rePassword: text })}
+                />
+                <TextInput 
+                    style={inputStyle} 
+                    placeholder="Enter your address" 
+                    value={this.state.address}
+                    secureTextEntry
+                    onChangeText={text => this.setState({ address: text })}
+                />
+                <TextInput 
+                    style={inputStyle} 
+                    placeholder="Enter your phone number" 
+                    value={this.state.phone}
+                    secureTextEntry
+                    onChangeText={text => this.setState({ phone: text })}
+                />
+                <TouchableOpacity style={bigButton}>
+                    <Text style={buttonText}>SIGN UP NOW</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    inputStyle: {
+        height: 50,
+        backgroundColor: '#fff',
+        marginBottom: 10,
+        borderRadius: 20,
+        paddingLeft: 30
+    },
+    bigButton: {
+        height: 50,
+        borderRadius: 20,
+        borderWidth: 1,
+        borderColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    buttonText: {
+        fontFamily: 'Avenir',
+        color: '#fff',
+        fontWeight: '400'
+    }
+});
